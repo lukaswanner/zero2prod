11db04cb7d98394259e9e9f5b52f408100a1d615
Lukas � <lukas_wanner@outlook.de>
2023-04-26 18:16:10 +0200

    feat: added .env file with database string for testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -42,7 +42,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let config = get_configuration().expect("Failed to read config!");
     let connection_string = config.database.connection_string();
 
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
 
@@ -61,6 +61,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 // parameterised test
