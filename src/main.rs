3e29717d5f469f2fb14770512046fd3ae191d0ce
Lukas🙏 <lukas.wanner@leicom.ch>
2023-04-27 12:47:25 +0200

    feat: added psql connection to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -5,9 +6,12 @@ use zero2prod::startup::run;
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::{healt_check, subscribe};
 // Notice the signature!
 // We return 'Server'on the happy path and we dont use async
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(healt_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
