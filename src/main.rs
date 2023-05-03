904eaa364f8336ddc51641ada5d0d98b65b898fd
Lukas🙏 <lukas.wanner@leicom.ch>
2023-05-03 12:46:55 +0200

    feat: main moved to tracing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
 
     let config = get_configuration().expect("Failed to read configuration");
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
