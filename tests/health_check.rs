8ab6e8178ce111273205382795fc9830604671ce
Lukas � <lukas_wanner@outlook.de>
2023-05-09 23:06:07 +0200

    feat: set output mode

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -14,8 +14,16 @@ pub struct TestApp {
 }
 
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 // Launch our application in the background
