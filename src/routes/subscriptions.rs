8bff9be9c13862154ad368c8f83b9e7a1ae2e523
Lukas � <lukas_wanner@outlook.de>
2023-05-22 16:00:48 +0200

    feat: insertion of new subscriber possible now

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -43,13 +43,7 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     }
 
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
+    if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
@@ -57,6 +51,30 @@ pub async fn subscribe(
     }
 
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(
+    name = "Send a conrifmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
+    let plain_body = &format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription",
+        confirmation_link
+    );
+    let html_body = &format!(
+        "Welcome to our newsletter! <br />\
+            Click <a href=\"{}\"> here </a> to confirm your subscription.",
+        confirmation_link
+    );
+
+    email_client
+        .send_email(new_subscriber.email, "Welcome", &plain_body, &html_body)
+        .await
 }
 
 #[tracing::instrument(
@@ -69,7 +87,7 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')"#,
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
