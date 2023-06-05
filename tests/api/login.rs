daa123e7a6f6a4bfca3d4ca25f505bb297bad931
Lukas � <lukas_wanner@outlook.de>
2023-06-05 19:35:25 +0200

    cookies introduced

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -11,7 +11,9 @@ async fn an_error_flash_message_is_set_on_failure() {
       "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
 
     // Assert
-    assert_is_redirect_to(&response, "/login")
+    assert_is_redirect_to(&response, "/login");
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
