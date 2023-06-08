89dbc843334f93dca6411da71d0b8edd092cdffa
Lukas � <lukas_wanner@outlook.de>
2023-06-08 23:19:02 +0200

    not much but honest work

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::{time::Duration, Cookie};
 use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
@@ -7,7 +8,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
             format!("<p><i>{}</i></p>", cookie.value())
         }
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -42,5 +43,11 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
   </form>
 </body>
 </html>"#
-        ))
+        ));
+
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+
+    response
 }
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -18,4 +18,8 @@ async fn an_error_flash_message_is_set_on_failure() {
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_login_html().await;
     assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
+
+    // Act - Part 3 - Reload the login page
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"Authentication failed"#));
 }
