09a2fd8a3309ff717933d8a4c4568014f327c5fa
Lukas � <lukas_wanner@outlook.de>
2023-06-13 12:29:52 +0200

    added password min, max length

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -65,3 +65,95 @@ async fn new_password_fields_must_match() {
     the field values must match.</i></p>"
     ))
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // Act - Part 1 - Login
+    app.post_login(&serde_json::json!({
+      "username": &app.test_user.username,
+      "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+                "current_password": &wrong_password,
+                "new_password": &new_password,
+                "new_password_check": &new_password,
+        }))
+        .await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"))
+}
+
+#[tokio::test]
+async fn password_is_too_short() {
+    // Arrange
+    let app = spawn_app().await;
+    let too_short = "tooshort123";
+
+    // Act - Part 1 - Login
+    app.post_login(&serde_json::json!({
+      "username": &app.test_user.username,
+      "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+                "current_password": &app.test_user.password,
+                "new_password": &too_short,
+                "new_password_check": &too_short,
+        }))
+        .await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(
+        html_page.contains("<p><i>The password is too short, use at least 12 characters.</i></p>")
+    )
+}
+
+#[tokio::test]
+async fn password_is_too_long() {
+    // Arrange
+    let app = spawn_app().await;
+    let too_long = String::from("x".repeat(129));
+
+    // Act - Part 1 - Login
+    app.post_login(&serde_json::json!({
+      "username": &app.test_user.username,
+      "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+                "current_password": &app.test_user.password,
+                "new_password": &too_long,
+                "new_password_check": &too_long,
+        }))
+        .await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(
+        html_page.contains("<p><i>The password is too long, use less than 128 characters.</i></p>")
+    )
+}
